6f52078e68f8e318cbbb6ebbc2bb60da25d74519
GuyTheDeveloper <99971488+GuyTheDeveloper@users.noreply.github.com>
2023-02-18 15:29:35 +0500

    Feat/registration (#9)
    * feat/bot start command
    
    * feat/start & registration start
    
    ---------
    
    Co-authored-by: Guy <efrat.gay@abutech.uz>

--- src/bot.js
+++ src/bot.js
@@ -1,4 +1,4 @@
-import { Telegraf } from "telegraf";
+import { Markup, Telegraf } from "telegraf";
 import pg from "pg";
 import { bot_token, connectionString } from "./config.js";
 
@@ -7,10 +7,32 @@ const pool = new pg.Pool({
   connectionString: connectionString,
 });
 
-bot.start((ctx) => {
-  ctx.reply("Pawol Naxuy");
-  ctx.reply("🖕");
+bot.start(async (ctx) => {
+  try {
+    const user = ctx.from;
+    const keyboard = Markup.keyboard(["Ro'yhatdan o'tish"]).resize();
+    ctx.reply("Assalomu Alaykum", keyboard);
+  } catch (error) {
+    ctx.reply("Aka uzur users relations yo'q");
+  }
 });
+
+bot.hears("Ro'yhatdan o'tish", async (ctx) => {
+  const user = ctx.from;
+
+  let oldUser = await pool.query("select * from users where user_id = $1", [
+    user.id,
+  ]);
+
+  if (oldUser.rowCount > 1) {
+    return ctx.reply(
+      `Assalomu Alaykum ${user.first_name}, siz allaqachon ro'yhatdan o'tgansiz`
+    );
+  }
+
+  ctx.reply("Tanlang", Markup.keyboard([["Usta", "Mijoz"]]).resize());
+});
+
 bot.help((ctx) => ctx.reply("Send me a sticker"));
 bot.on("sticker", (ctx) => ctx.reply("👍"));
 bot.hears("hi", (ctx) => ctx.reply("Hey there"));
